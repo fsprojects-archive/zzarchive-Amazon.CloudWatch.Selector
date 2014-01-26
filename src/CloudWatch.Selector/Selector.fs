ffaeb970d659497dc09694f597599401688fcc57
theburningmonk <theburningmonk@gmail.com>
2014-01-26 23:29:05 +0000

    - added external dsl

--- src/CloudWatch.Selector/Selector.fs
+++ src/CloudWatch.Selector/Selector.fs
@@ -2,6 +2,7 @@
 
 open System
 open System.Collections.Generic
+open System.Globalization
 open System.Runtime.CompilerServices
 open System.Text.RegularExpressions
 
@@ -65,13 +66,17 @@ module Model =
                            "Count/Second"; "None" ]
 
 [<AutoOpen>]
+module Utils =
+    let inline lowerCase (str : string)          = str.ToLower()
+    let inline isRegexMatch pattern input        = Regex.IsMatch(input, pattern)
+
+    let inline eqFilter filter term lf           = filter <| (term, fun rt -> lowerCase lf = lowerCase rt)
+    let inline regexFilter filter term pattern   = filter <| (term, fun input -> isRegexMatch (lowerCase pattern) (lowerCase input))
+
+    let inline statsFilter term op value = StatsFilter <| (term, fun x -> op x value)
+
+[<AutoOpen>]
 module InternalDSL = 
-    let private lowerCase (str : string) = str.ToLower()
-    let private isRegexMatch pattern input = Regex.IsMatch(input, pattern)
-
-    let inline private eqFilter filter term lf = filter <| (term, fun rt -> lowerCase lf = lowerCase rt)
-    let inline private regexFilter filter term pattern = filter <| (term, fun input -> isRegexMatch (lowerCase pattern) (lowerCase input))
-
     let namespaceIs ns        = eqFilter MetricFilter Namespace ns
     let namespaceLike pattern = regexFilter MetricFilter Namespace pattern
 
@@ -79,8 +84,7 @@ module InternalDSL =
     let nameLike pattern      = regexFilter MetricFilter Name pattern
 
     let unitIs unit           = eqFilter UnitFilter Unit unit
-
-    let inline private statsFilter term op value = StatsFilter <| (term, fun x -> op x value)
+    
     let average op value      = statsFilter Average op value
     let min op value          = statsFilter Min op value
     let max op value          = statsFilter Max op value
@@ -101,7 +105,161 @@ module InternalDSL =
 
 [<AutoOpen>]
 module ExternalDSL =
-    ()
+    let inline minutes n  = n |> float |> TimeSpan.FromMinutes
+    let inline hours n    = n |> float |> TimeSpan.FromHours
+    let inline days n     = n |> float |> TimeSpan.FromDays
+    let timestampFormat   = "yyyy-MM-dd HH:mm:ss"
+
+    let (|StringCI|_|) (str : string) input = 
+        if str.Equals(input, StringComparison.CurrentCultureIgnoreCase) then Some () else None
+
+    let (|EmptyString|_|) input = if String.IsNullOrWhiteSpace input then Some () else None
+
+    let (|StartsWith|_|) char (input : string) = if input.[0] = char then Some () else None
+    let (|EndsWith|_|) char (input : string)   = if input.[input.Length-1] = char then Some () else None
+
+    let (|QuotedString|_|) (input : string) =
+        match input with
+        | EmptyString -> None
+        | str when str.Length < 2 -> None
+        | StartsWith ''' & EndsWith ''' -> Some <| input.Substring(1, input.Length - 2)
+
+    let (|Float|) input = 
+        match System.Double.TryParse input with
+        | true, n -> n
+        | _       -> failwithf "Unexpected token [%s], expecting a floating number." input
+
+    let (|Operator|) input : float -> 'a -> bool = 
+        match input with
+        | "="  -> (=)
+        | ">=" -> (>=)
+        | ">"  -> (>)
+        | "<=" -> (<=)
+        | "<"  -> (<)
+        | x    -> failwithf "Unexpected token [%s], expecting =, >=, >, <= or <." x
+
+    let inline (|Minutes|Hours|Days|) input = 
+        match input with
+        | StringCI "Minutes" -> Minutes minutes
+        | StringCI "Hours"   -> Hours hours
+        | StringCI "Days"    -> Days days
+        | x                  -> failwithf "Unexpected token [%s], expecting 'minutes', 'hours' or 'days'." x
+
+    let (|TimeStamp|) input =
+        match input with
+        | QuotedString str ->
+            match DateTime.TryParseExact(str, timestampFormat, CultureInfo.CurrentCulture, DateTimeStyles.None) with
+            | true, dt -> Some dt
+            | _ -> None
+        | _ -> None
+        |> function 
+            | Some x -> x 
+            | _ -> failwithf "Token [%s] is not a valid timestamp, expecting a timestamp in format '%s'." input timestampFormat
+
+    let (|And|_|) = function | StringCI "and"::tl -> Some(tl) | _ -> None
+
+    let (|At|_|) = function | StringCI "at"::tl -> Some(tl) | _ -> None
+
+    let (|NamespaceIs|_|) = function
+        | StringCI "NamespaceIs"::QuotedString ns::tl
+            -> Some(eqFilter MetricFilter Namespace ns, tl)
+        | _ -> None
+
+    let (|NamespaceLike|_|) = function
+        | StringCI "NamespaceLike"::QuotedString pattern::tl 
+            -> Some(regexFilter MetricFilter Namespace pattern, tl)
+        | _ -> None
+
+    let (|NameIs|_|) = function
+        | StringCI "NameIs"::QuotedString n::tl 
+            -> Some(eqFilter MetricFilter Name n, tl)
+        | _ -> None
+
+    let (|NameLike|_|) = function
+        | StringCI "NameLike"::QuotedString pattern::tl 
+            -> Some(regexFilter MetricFilter Name pattern, tl)
+        | _ -> None
+
+    let (|UnitIs|_|) = function
+        | StringCI "UnitIs"::QuotedString unit::tl
+            -> Some(eqFilter UnitFilter Unit unit, tl)
+        | _ -> None
+
+    let (|Average|_|) = function
+        | StringCI "Average"::Operator op::Float value::tl -> Some(statsFilter StatsTerm.Average op value, tl)
+        | _ -> None
+
+    let (|Min|_|) = function
+        | StringCI "Min"::Operator op::Float value::tl -> Some(statsFilter StatsTerm.Min op value, tl)
+        | _ -> None
+
+    let (|Max|_|) = function
+        | StringCI "Max"::Operator op::Float value::tl -> Some(statsFilter StatsTerm.Max op value, tl)
+        | _ -> None
+
+    let (|Sum|_|) = function
+        | StringCI "Sum"::Operator op::Float value::tl -> Some(statsFilter StatsTerm.Sum op value, tl)
+        | _ -> None
+
+    let (|SampleCount|_|) = function
+        | StringCI "SampleCount"::Operator op::Float value::tl -> Some(statsFilter StatsTerm.SampleCount op value, tl)
+        | _ -> None
+
+    let (|DuringLast|_|) = function
+        | StringCI "DuringLast"::Float n::(Minutes unit|Hours unit|Days unit)::tl 
+            -> Some(unit n |> Last, tl)
+        | _ -> None
+
+    let (|Since|_|) = function 
+        | StringCI "Since"::TimeStamp timestamp::tl -> Some(TimeFrame.Since timestamp, tl)
+        | _ -> None
+
+    let (|Between|_|) = function
+        | StringCI "Between"::TimeStamp startTime::TimeStamp endTime::tl 
+            -> Some(TimeFrame.Between(startTime, endTime), tl)
+        | _ -> None
+
+    let (|IntervalOf|_|) = function
+        | StringCI "IntervalOf"::Float n::(Minutes unit|Hours unit|Days unit)::tl
+            -> Some(unit n |> Period, tl)
+        | _ -> None
+
+    let parseFilter tokens = 
+        let rec loop acc = function
+            | NamespaceIs   (filter, tl) | NamespaceLike (filter, tl) 
+            | NameIs        (filter, tl) | NameLike      (filter, tl)
+            | UnitIs        (filter, tl)
+            | Average       (filter, tl) | Sum           (filter, tl)
+            | Min           (filter, tl) | Max           (filter, tl)
+            | SampleCount   (filter, tl)
+                -> match tl with
+                   | And tl -> loop (filter::acc) tl
+                   | _ -> (filter::acc) |> List.reduce (+), tl
+            | _ -> failwith "Missing filters. You need to specify at least one filter on the metric or stats."
+
+        loop [] tokens
+
+    let parseTimeFrame (filter, tokens) = 
+        match tokens with
+        | DuringLast    (timeframe, tl)
+        | Since         (timeframe, tl)
+        | Between       (timeframe, tl)
+            -> { Filter = filter; TimeFrame = timeframe; Period = None }, tl
+        | _ -> failwith "Missing timeframe. You need to specify a timeframe for the query with 'duringLast', 'since' or 'between'."
+
+    let parsePeriod (query, tokens) =
+        match tokens with
+        | At tl -> 
+            match tl with
+            | IntervalOf (period, _) -> { query with Period = Some period }
+            | _ -> failwithf "Missing period. Expecting 'intervalOf' after 'at'."
+        | _ -> query
+
+    let parse (input : string) =
+        input.Split(' ') |> List.ofArray
+        |> parseFilter
+        |> parseTimeFrame
+        |> parsePeriod
 
 [<AutoOpen>]
 module Execution = 
@@ -161,11 +319,11 @@ module Execution =
             match filter with
             | StatsFilter (term, pred) ->
                 match term with
-                | Average     -> fun (dp : Datapoint) -> pred dp.Average
-                | Min         -> fun (dp : Datapoint) -> pred dp.Minimum
-                | Max         -> fun (dp : Datapoint) -> pred dp.Maximum
-                | Sum         -> fun (dp : Datapoint) -> pred dp.Sum
-                | SampleCount -> fun (dp : Datapoint) -> pred dp.SampleCount
+                | StatsTerm.Average     -> fun (dp : Datapoint) -> pred dp.Average
+                | StatsTerm.Min         -> fun (dp : Datapoint) -> pred dp.Minimum
+                | StatsTerm.Max         -> fun (dp : Datapoint) -> pred dp.Maximum
+                | StatsTerm.Sum         -> fun (dp : Datapoint) -> pred dp.Sum
+                | StatsTerm.SampleCount -> fun (dp : Datapoint) -> pred dp.SampleCount
                 |> (<?&>) acc
                 |> Some
             | CompositeFilter (lf, rt) ->
@@ -204,9 +362,9 @@ module Execution =
             let now = DateTime.UtcNow
             let startTime, endTime = 
                 match timeframe with
-                | Last ts       -> now.Add(-ts), now
-                | Since dt      -> dt, now
-                | Between(a, b) -> a, b
+                | TimeFrame.Last ts       -> now.Add(-ts), now
+                | TimeFrame.Since dt      -> dt, now
+                | TimeFrame.Between(a, b) -> a, b
             req.StartTime <- startTime
             req.EndTime   <- endTime
 
