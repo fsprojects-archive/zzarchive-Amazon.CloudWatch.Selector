0a531517b936c90f02be483dad48e4bb18a5a61b
theburningmonk <theburningmonk@gmail.com>
2014-03-05 23:39:11 +0000

    - closes #13 - closes #11 - closes #9

--- src/CloudWatch.Selector.Cli/Program.fs
+++ src/CloudWatch.Selector.Cli/Program.fs
@@ -2,6 +2,7 @@
 open System.Collections.Generic
 open System.Diagnostics
 open System.IO
+open System.Reflection
 open Microsoft.FSharp.Text
 
 open Amazon
@@ -13,12 +14,20 @@ let stringCI text (input : string) = input.Equals(text, StringComparison.Current
 
 let (|Exit|_|) input = if stringCI "exit" input then Some () else None
 let (|Plot|_|) input = if stringCI "plot" input then Some () else None
+let (|Help|_|) input = if stringCI "help" input then Some () else None
 
 let stringJoin separator (seq : string seq) = String.Join(separator, seq)
 let csv             = stringJoin ","
 let singleQuote     = sprintf "'%s'"
 let bracket         = sprintf "[%s]"
 let trailingComma   = sprintf "%s,"
+let combinePath a b = Path.Combine(a, b)
+let tempPath        = Path.GetTempPath()
+
+let resultTemplate = 
+    use stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Result.html")
+    use reader = new StreamReader(stream)
+    reader.ReadToEnd()
 
 let draw (state : (Metric * List<Datapoint>)[]) =
     let data = new Dictionary<DateTime, Dictionary<string, Datapoint>>()
@@ -54,21 +63,21 @@ let draw (state : (Metric * List<Datapoint>)[]) =
                 yield timestamp::line |> csv |> bracket |> trailingComma
         }
         |> stringJoin "\n"
-    
-    let template = ref <| File.ReadAllText("Result.html")
 
-    let replaceToken (token : string) f =
+    let replaceToken (token : string) f (output : string) =
         let data = getData f
-        template := (!template).Replace(token, data)
+        output.Replace(token, data)
 
-    replaceToken "@avg_data@" (fun dp -> dp.Average)
-    replaceToken "@min_data@" (fun dp -> dp.Minimum)
-    replaceToken "@max_data@" (fun dp -> dp.Maximum)
-    replaceToken "@sum_data@" (fun dp -> dp.Sum)
-    replaceToken "@sample_count_data@" (fun dp -> dp.SampleCount)
+    let output = 
+        resultTemplate
+        |> replaceToken "@avg_data@" (fun dp -> dp.Average)
+        |> replaceToken "@min_data@" (fun dp -> dp.Minimum)
+        |> replaceToken "@max_data@" (fun dp -> dp.Maximum)
+        |> replaceToken "@sum_data@" (fun dp -> dp.Sum)
+        |> replaceToken "@sample_count_data@" (fun dp -> dp.SampleCount)
 
-    let outputFile = sprintf "Result_%s.html" <| DateTime.UtcNow.ToString("yyyyMMdd_HHmmss")
-    File.WriteAllText(outputFile, !template)
+    let outputFile = sprintf "Result_%s.html" <| DateTime.UtcNow.ToString("yyyyMMdd_HHmmss") |> combinePath tempPath
+    File.WriteAllText(outputFile, output)
 
     Process.Start(outputFile) |> ignore
 
@@ -78,6 +87,15 @@ let rec loop (cloudWatch : IAmazonCloudWatch) (state : (Metric * List<Datapoint>
     printf "%s" prefix
     match Console.ReadLine() with
     | Exit  -> printfn "%sGood bye!" prefix
+    | Help  -> printfn """%sCommands:
+    help        - display list of commands
+    exit        - exit the CLI
+    plot        - plots the current results onto a set of graphs
+
+All other inputs are treated as a query, go to http://bit.ly/awscwcli to learn more about the DSL syntax and find examples.
+    """
+                       prefix
+               loop cloudWatch state
     | Plot  -> 
         match state with
         | Some state' -> 
